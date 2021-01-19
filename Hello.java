60196a122e323b7bdce17c0d9a0262bb5254c010
Tharun Pothuraj <tharunkumar1490@gmail.com>
2021-01-19 16:44:11 +0530

    Update Hello.java
    Added 6 more print statements.

--- Hello.java
+++ Hello.java
@@ -1,5 +1,11 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("This is first line.");
+        System.out.println("This is second line.");
+        System.out.println("This is third line.");
+        System.out.println("This is fourth line.");
+        System.out.println("This is fifth line.");
+        System.out.println("This is sixth line.");
+        System.out.println("This is seventh line.");
     }
 }
